1e1bf8e9e3e2b3b3603f7e313abb2b9ba3798bd7
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-12 19:49:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday, items|
-      puts "  #{holiday.to_s.split("_").each { |e| e.capitalize }.join(" ")}: #{items.join(", ")}"
+      puts "  #{holiday.to_s.split("_").each { |e| e.capitalize! }.join(" ")}: #{items.join(", ")}"
     end
   end
 end
